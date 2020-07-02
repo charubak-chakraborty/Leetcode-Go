fb988d760531d5dc8b42bc60cbaeade3b52247cd
Charubak Chakraborty <charubak.chakraborty@gmail.com>
2020-07-02 22:11:53 +0530

    1431 and 1470 solution added

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 )
 
 func main() {
-	nums := []int{1, 2, 3, 4}
-	output := leetcodeArray.RunningSum(nums)
+	// 1480
+	// nums := []int{1, 2, 3, 4}
+	// output := leetcodeArray.RunningSum(nums)
+	// fmt.Print(output)
+
+	// 1470
+	// nums := []int{1, 1, 2, 2}
+	// output := leetcodeArray.Shuffle(nums, 2)
+	// fmt.Print(output)
+
+	// 1431
+	candies := []int{2, 3, 5, 1, 3}
+	extraCandies := 3
+	output := leetcodeArray.KidsWithCandies(candies, extraCandies)
 	fmt.Print(output)
 }
